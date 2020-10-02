d37d4cdf3514e8611cb26c46765d34e28bf32e18
aurumcodex <aurumcodex@protonmail.com>
2020-10-01 22:33:47 -0400

    change default branch name to main

--- util/jdks.go
+++ util/jdks.go
@@ -1,13 +1,37 @@
 package util
 
 type Oracle struct {
-	Base     string `toml:base_url`
+	Base string `toml:base_url`
+	Data OracleInfo
+}
+
+type OracleInfo struct {
 	Ext      string `toml:ext_url`
 	Filename string `toml:filename`
 }
 
 type Corretto struct {
-	Base     string `toml:base_url`
+	Base string `toml:base_url`
+	Data CorrettoInfo
+}
+
+type CorrettoInfo struct {
 	Filename string `toml:filename`
 	File     string `toml:file`
+}
+
+type Liberica struct {
+	Base string `toml:base_url`
+	Data LibericaInfo
+}
+
+type LibericaInfo struct {
+	Ext      string `toml:ext_url`
+	Filename string `toml:filename`
+}
+
+type OpenJDK struct {
+}
+
+type OpenJDKInfo struct {
 }
